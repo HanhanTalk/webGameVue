2cdf63c798a71bf834e5a44d92c61d65fb47f38e
mpr0xy <mp@mpr0xy.com>
2017-07-24 22:48:42 +0800

    add

--- server/data/db.js
+++ server/data/db.js
@@ -1,14 +1,21 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/webgame', {
-  promiseLibrary: global.Promise,
-  useMongoClient: true});
+mongoose.connect('mongodb://localhost/webgame', 
+  {
+    promiseLibrary: global.Promise,
+    useMongoClient: true
+  }
+);
 
 
 var User = mongoose.model('user', { 
   name: String,
   password: String,
-  nick: String
+  nick: String,
+  ulevel: {type: Number, default: 1},
+  gold: {type: Number, default: 0},
+  flower: {type: Number, default: 0},
+  portrait: {type: String, default: ''}
 });
 
 
---
+++ server/util/response.js
@@ -0,0 +1,17 @@
+var _ = require('lodash');
+
+/**
+ * 以后处理用户信息的返回
+ * @param {*} userInfo 
+ */
+function userInfo(userInfo) {
+  if (!userInfo.nick) {
+    userInfo.nick = userInfo.name;
+  }
+  userInfo.accout = userInfo.name;
+  return userInfo;
+}
+
+module.exports = {
+  userInfo: userInfo
+}
